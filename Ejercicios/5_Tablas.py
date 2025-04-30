4c045f2bd8c98f2d29dd31b40f1eb5f78da3fcf4
leo <leoanrdomule@gmail.com>
2025-04-30 15:56:38 -0300

    30/4/2025

--- Ejercicios/5_Tablas.py
+++ Ejercicios/5_Tablas.py
@@ -1,11 +1,9 @@
-"""
-Le preguntamos al usuario qué tabla quiere que le mostremos
-"""
+# Le preguntamos al usuario qué tabla quiere que le mostremos
 
 print("¿Qué tabla te gustaría ver? ")
 tabla = int(input("Respuesta: "))
 
-for i in range(1, 11):
+for i in range(11):
     respuesta = i * tabla
     print(f"{tabla} x {i} = {respuesta}")
     i += 1
